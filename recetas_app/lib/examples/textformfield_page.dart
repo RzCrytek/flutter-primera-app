b6dec8f53209fbf1cfaf6d3adbd8fdf31756d362
IVAN FALCON <ivan.falcon@elektra.com.mx>
2026-09-03 19:52:19 -0500

    feat: agrego validacion de correo y campo contraseña

--- recetas_app/lib/examples/textformfield_page.dart
+++ recetas_app/lib/examples/textformfield_page.dart
@@ -4,7 +4,8 @@ class TextformfieldPage extends StatelessWidget {
   TextformfieldPage({super.key});
 
   final _formKey = GlobalKey<FormState>();
-  TextEditingController _controller = TextEditingController();
+  TextEditingController _emailController = TextEditingController();
+  TextEditingController _passwordController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
@@ -19,10 +20,11 @@ class TextformfieldPage extends StatelessWidget {
               mainAxisSize: MainAxisSize.min,
               children: [
                 TextFormField(
+                  controller: _emailController,
                   style: TextStyle(
                     color: Colors.blue,
                     fontSize: 18,
-                    fontWeight: .bold,
+                    fontWeight: FontWeight.bold,
                   ),
                   cursorColor: Colors.red, //Colors del cursor
                   cursorWidth: 10, //ancho del cursos
@@ -62,6 +64,70 @@ class TextformfieldPage extends StatelessWidget {
                       borderRadius: BorderRadius.circular(25),
                     ),
                   ),
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return "El campo no puede estar vacío";
+                    } else if (value.length < 5) {
+                      return "El correo debe tener al menos 5 caracteres";
+                    } else if (value.length > 20) {
+                      return "El correo no puede tener más de 20 caracteres";
+                    } else if (!RegExp(
+                      r'^[^@]+@[^@]+\.[^@]+$',
+                    ).hasMatch(value)) {
+                      return "El correo no es válido";
+                    } else {
+                      return null;
+                    }
+                  },
+                ),
+                SizedBox(height: 30),
+                TextFormField(
+                  controller: _passwordController,
+                  obscureText: true,
+                  decoration: InputDecoration(
+                    labelText: "Contraseña",
+                    filled: true,
+                    fillColor: Colors.grey.shade300,
+                    hintText: "********",
+                    hintStyle: TextStyle(fontWeight: FontWeight.w300),
+                    helperText: "Ingresa tu Contraseña ",
+                    prefix: Icon(Icons.lock),
+                    suffixIcon: Icon(Icons.check_circle),
+                    border: OutlineInputBorder(borderSide: BorderSide.none),
+                  ),
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return "El campo no puede estar vacío";
+                    } else if (value.length < 5) {
+                      return "La contraseña debe tener al menos 5 caracteres";
+                    } else if (value.length > 10) {
+                      return "La contraseña no puede tener más de 10 caracteres";
+                    } else {
+                      return null;
+                    }
+                  },
+                ),
+                ElevatedButton(
+                  onPressed: () {
+                    if (_formKey.currentState!.validate()) {
+                      ScaffoldMessenger.of(context).showSnackBar(
+                        SnackBar(
+                          content: Text("Formulario enviado"),
+                          backgroundColor: Colors.green,
+                          duration: Duration(seconds: 2),
+                        ),
+                      );
+                    } else {
+                      ScaffoldMessenger.of(context).showSnackBar(
+                        SnackBar(
+                          content: Text("Revisa las alertas del formulario"),
+                          backgroundColor: Colors.red,
+                          duration: Duration(seconds: 2),
+                        ),
+                      );
+                    }
+                  },
+                  child: Text("Enviar Formulario"),
                 ),
               ],
             ),
