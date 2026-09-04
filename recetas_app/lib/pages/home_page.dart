b7ca317247d9d7ff17d0266026e09957aaf0f61d
IVAN FALCON <ivan.falcon@elektra.com.mx>
2026-09-03 21:58:19 -0500

    feat: agrego receta al container

--- recetas_app/lib/pages/home_page.dart
+++ recetas_app/lib/pages/home_page.dart
@@ -1,14 +1,25 @@
 import 'package:flutter/material.dart';
+import 'package:recetas_app/models/receta_model.dart';
 import 'package:recetas_app/widget/form_item_widget.dart';
 import 'package:recetas_app/widget/receta_card_widget.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   HomePage({super.key});
 
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
   final _formKey = GlobalKey<FormState>();
-  final TextEditingController _titleController = TextEditingController();
-  final TextEditingController _preparationController = TextEditingController();
-  final TextEditingController _imageController = TextEditingController();
+
+  TextEditingController _titleController = TextEditingController();
+
+  TextEditingController _preparationController = TextEditingController();
+
+  TextEditingController _imageController = TextEditingController();
+
+  List<RecetaModel> recetasList = [];
 
   @override
   Widget build(BuildContext context) {
@@ -29,26 +40,71 @@ class HomePage extends StatelessWidget {
                 controller: _titleController,
                 iconData: Icons.title,
                 label: "Ingresa el título",
+                validator: (p0) {
+                  if (p0 == null || p0.isEmpty) {
+                    return "return el título es obligatorio";
+                  } else if (p0.length < 3) {
+                    return "El título debe tener mínimo 3 caracteres";
+                  } else {
+                    return null;
+                  }
+                },
               ),
               FormItemWidget(
                 controller: _preparationController,
                 iconData: Icons.list,
                 label: "Ingresa pa preparación",
                 maxLines: 3,
+                validator: (p0) {
+                  if (p0 == null || p0.isEmpty) {
+                    return "La prepararación es obligatorio";
+                  } else if (p0.length < 10) {
+                    return "El título debe tener mínimo 10 caracteres";
+                  } else {
+                    return null;
+                  }
+                },
               ),
               FormItemWidget(
                 controller: _imageController,
                 iconData: Icons.image,
                 label: "Ingresa la url de imagen",
+                validator: (p0) {
+                  if (p0 == null || p0.isEmpty) {
+                    return "La imagen es obligatoria";
+                  } else if (!p0.startsWith("http")) {
+                    return "Ingresa un url válida";
+                  } else {
+                    return null;
+                  }
+                },
               ),
               Padding(
                 padding: const EdgeInsets.symmetric(vertical: 16),
                 child: ElevatedButton(
-                  onPressed: () {},
+                  onPressed: () {
+                    if (_formKey.currentState!.validate()) {
+                      print("Formulario válido");
+                      RecetaModel recetaModelAux = RecetaModel(
+                        title: _titleController.text,
+                        preparation: _preparationController.text,
+                        urlImage: _imageController.text,
+                      );
+                      recetasList.add(recetaModelAux);
+                      _titleController.clear();
+                      _preparationController.clear();
+                      _imageController.clear();
+                      setState(() {});
+                    } else {
+                      print("Formulario inválido");
+                    }
+                  },
                   child: Text("Registrar receta"),
                 ),
               ),
-              RecetaCardWidget(),
+              ...recetasList.map((receta) {
+                return RecetaCardWidget(recetaModel: receta);
+              }).toList(),
             ],
           ),
         ),
